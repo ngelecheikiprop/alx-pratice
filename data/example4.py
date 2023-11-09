76391c56bba4e4e86d1e7f1d8895010e628152b8
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-09 03:32:51 +0300

    make a list of functions using lambda

--- data/example4.py
+++ data/example4.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
+
+#---------sum  lambda function--------
 sum  = lambda x, y : x + y
 print ("2 + 2  = {}".format(sum(2,2)))
+
+#---can vote lambada function-----
+can_vote = lambda age: True if age >= 18 else False
+print("can vote :{}".format(can_vote(10)))
+
+#-----list of functions-------------
+powerList = [lambda x: x**2,
+		lambda x: x**3,
+		lambda x: x**4,
+		lambda x: x**5]
+
+for func in powerList:
+	print(func(4))
+
