f6332c01bf2af63be3d008f052c1ad18dfa78b95
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-09 03:09:48 +0300

    made a random function

---
+++ data/example3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def random_func(name: str, age: int weight: float) -> str:
+	print("Name : {}".format(name))
+	print("Age : {}".format(age))
+	print("Weight : {}".format(weight))
+
+	return "{} is {} and weighs {}".format(name,age, weight)
+
+print(rand_func("Kiprop", 41, 165.5)
+
